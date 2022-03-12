55df53fe5239608fc388dd3845268016d60a3db1
xElon-Mask <xelonmask@protonmail.com>
2022-03-12 23:50:43 +0100

    reusable fonction for any eth address

--- verif.sol
+++ verif.sol
@@ -1,10 +1,9 @@
-//SPDX License Identifier: GPL-3.0 
+//SPDX-License-Identifier: GPL-3.0 
 
 pragma solidity ^0.8.12;
 
 contract verif {
     function getBalance(address addr) public view returns (uint) {
-        address addr=0xa43F97DD48992CEe3C8b91f5c8DFDec3a1731746;
         return addr.balance;
     }
 }
